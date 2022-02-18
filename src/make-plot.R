afa281661373e4546221a878d6158423f084e376
louise2111 <99971291+louise2111@users.noreply.github.com>
2022-02-18 16:06:33 +0000

    Create make-plot.R

---
+++ src/make-plot.R
@@ -0,0 +1,40 @@
+dir.create()
+
+install.packages("tidyverse")
+library(tidyverse)
+library(ggplot2)
+
+sods_data <- read.csv("/Users/louisemiller/Documents/documents/data/stackoverflow-git-data.csv")
+g <- ggplot(data = sods_data,
+            mapping = aes(x = year, y = percentage))+
+  geom_point()+
+  geom_smooth(method = "lm")+
+  geom_text(aes(x = 2020, y = 82.8, label = "only GitHub"),
+             nudge_x = 0.2,
+             nudge_y = -4) +
+  labs(
+    x = "Year",
+    y = "Percentage who used git",
+    title = "Git usage has increased",
+    subtitle = "Data from Stackoverflow Developer Survey")
+
+g
+
+ggsave(filename = "git-usage.png",
+       plot = g,
+       height = 7.4,
+       width = 10.5,
+       units = "cm")
+
+sink(file = "regression-summary.txt")
+summary(lm(percentage ~ year, data = sods_data))
+sink()
+
+ggsave(filename = "out/git-usuage.png",
+       plot = g,
+       height = 7.4,
+       width = 10.5,
+       units = "cm")
+sink(file = "out/regression-summary.txt")
+summary(lm(percentage ~ year, data = sods_data))
+sink()
